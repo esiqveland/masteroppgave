4529c518545b245338103fad00fb6aee2fce54e8
Eivind Siqveland Larsen <to.eivind@gmail.com>
2014-01-27 17:55:24 +0100

    fix boot up

--- voldemort/boot_pinode.py
+++ voldemort/boot_pinode.py
@@ -16,8 +16,8 @@ prop_file = "config/picluster/config/server.properties"
 os.chdir(prosjektdir)
 
 def update_git():
+    subprocess.call(["git", "checkout", "."])
     subprocess.call(["git", "pull"])
-    subprocess.call(["make"])
 
 def launch_voldemort():
     subprocess.call(["voldemort-pi.sh"])
@@ -31,9 +31,9 @@ def set_node_id(property_file, node_id):
         else:
             print line,
 
-set_node_id(property_file=prop_file, node_id=node_number)
 
 while True:
-    #update_git() # will not work
+    update_git() # will not work
+    set_node_id(property_file=prop_file, node_id=node_number)
     launch_voldemort()
 
