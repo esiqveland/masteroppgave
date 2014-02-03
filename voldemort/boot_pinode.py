e9358f3fc3c1c80376cd9301b872e6f1e3072a8c
Eivind Siqveland Larsen <to.eivind@gmail.com>
2014-02-03 14:15:01 +0100

    boot_pinode: variable misspelled

--- voldemort/boot_pinode.py
+++ voldemort/boot_pinode.py
@@ -16,7 +16,7 @@ prop_file = "config/picluster/config/server.properties"
 os.chdir(prosjektdir)
 
 def update_git():
-    subprocess.call(["git", "checkout", propfile])
+    subprocess.call(["git", "checkout", prop_file])
     subprocess.call(["git", "pull"])
 
 def launch_voldemort():
