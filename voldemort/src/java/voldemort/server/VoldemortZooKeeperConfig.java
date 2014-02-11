b0ff58e931a267b6d645854d6da9dd26e267c51f
Eivind Siqveland Larsen <to.eivind@gmail.com>
2014-02-11 16:27:54 +0100

    First try at rewriting for ZooKeeper configuration.
    The idea is subclassing VoldemortConfig and setup the class with data from ZooKeeper.
    The design is however questionable and should be reconsidered.
    
    I am also not sure at current time how we should send the ZooKeeper startup string,
    but it will probably be as command line parameter or some small config file.
    This will probably have the format zk:hostname:port/path/to/chroot/to

---
+++ voldemort/src/java/voldemort/server/VoldemortZooKeeperConfig.java
@@ -0,0 +1,95 @@
+package voldemort.server;
+
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+import voldemort.utils.ConfigurationException;
+import voldemort.utils.Props;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Properties;
+
+public class VoldemortZooKeeperConfig extends VoldemortConfig implements Watcher {
+
+    private ZooKeeper zk = null;
+    private String zkURI = "localhost:3000";
+
+    public VoldemortZooKeeperConfig(ZooKeeper zk, Props properties) throws ConfigurationException {
+        super(properties);
+        this.zk = zk;
+    }
+
+    private static Props loadConfigs(ZooKeeper zk) {
+
+        Props properties = null;
+
+        try {
+            String nodeproperties = getNodeConfigFromZooKeeper(zk);
+            //String clusterxml = getClusterConfigFromZooKeeper(zk);
+
+            Properties propertiesData = new Properties();
+            propertiesData.load(new StringReader(nodeproperties));
+            properties = new Props(propertiesData);
+
+        } catch(IOException e) {
+            throw new ConfigurationException("Error reading configs from ZooKeeper",e);
+        }
+        return properties;
+    }
+
+    public static VoldemortConfig loadFromZooKeeper(String zookeeperurl) {
+        ZooKeeper zk = setupZooKeeper(zookeeperurl, null);
+
+        Props props = loadConfigs(zk);
+
+        VoldemortZooKeeperConfig voldemortConfig = new VoldemortZooKeeperConfig(zk, props);
+        return voldemortConfig;
+    }
+
+    private static String getClusterConfigFromZooKeeper(ZooKeeper zk) {
+        return getFileFromZooKeeper(zk, "/voldemort/config/cluster.xml");
+    }
+
+    private static String getFileFromZooKeeper(ZooKeeper zk, String path) {
+        Stat stat = new Stat();
+        String s = null;
+        try {
+            byte[] configdata = zk.getData(path, false, stat);
+            s = new String(configdata);
+        } catch (KeeperException e) {
+            throw new RuntimeException(String.format("Error getting key from ZooKeeper: %s", path), e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(String.format("Error getting key from ZooKeeper: %s", path), e);
+        }
+        return s;
+    }
+
+    public static ZooKeeper setupZooKeeper(String zkURI, Watcher callback) {
+        ZooKeeper zk = null;
+        if(zk == null || !zk.getState().isAlive()) {
+            try {
+                zk = new ZooKeeper(zkURI, 3000, callback);
+            } catch (IOException e) {
+                throw new ConfigurationException("Error setting up ZooKeeper object!", e);
+            }
+        }
+        return zk;
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+
+    }
+
+    public static String getNodeConfigFromZooKeeper(ZooKeeper zk) throws UnknownHostException {
+        String hostname = new String(InetAddress.getLocalHost().getCanonicalHostName().toString());
+
+        return getFileFromZooKeeper(zk, "/voldemort/config/"+hostname+".properties");
+    }
+}
