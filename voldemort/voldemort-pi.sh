f0f91d8148c601b8c091583d842cc6e623c9be17
Eivind Siqveland Larsen <to.eivind@gmail.com>
2014-02-13 18:44:55 +0100

    voldemort change launch script

--- voldemort/voldemort-pi.sh
+++ voldemort/voldemort-pi.sh
@@ -2,13 +2,13 @@
 
 export VOLD_HOME="/home/king/src/masteroppgave/voldemort"
 
-#export VOLDEMORT_HOME="${VOLD_HOME}/config/maccluster/config"
-export VOLDEMORT_HOME="zk:192.168.0.210/voldemort"
+export VOLDEMORT_HOME="${VOLD_HOME}/config/maccluster/config"
+export ZOOKEEPER_VOLDEMORT="zk:192.168.0.210/voldemort"
 
 echo "Running from dir: "
 echo ${VOLDEMORT_HOME}
 
 screen -wipe
 
-screen -S voldemort -d -m /bin/bash -c "./bin/voldemort-piserver.sh $VOLD_HOME $VOLDEMORT_HOME; exec bash"
+screen -S voldemort -d -m /bin/bash -c "./bin/voldemort-piserver.sh $VOLD_HOME $VOLDEMORT_HOME $ZOOKEEPER_VOLDEMORT ; exec bash"
 
