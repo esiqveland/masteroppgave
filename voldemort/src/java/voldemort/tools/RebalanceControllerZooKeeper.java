049b70284f6cdded4d2dca52cbd3c724d226c50b
knudix <knutnyg@gmail.com>
2014-02-26 19:17:57 +0100

    added a few log lines and updated rebalancecontrollerZookeepere

--- voldemort/src/java/voldemort/tools/RebalanceControllerZooKeeper.java
+++ voldemort/src/java/voldemort/tools/RebalanceControllerZooKeeper.java
@@ -1,16 +1,22 @@
 package voldemort.tools;
+import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
+import voldemort.client.protocol.admin.AdminClient;
 import voldemort.client.rebalance.RebalanceController;
 import voldemort.client.rebalance.RebalancePlan;
 import voldemort.cluster.Cluster;
+import voldemort.cluster.Node;
 import voldemort.store.StoreDefinition;
 import voldemort.utils.RebalanceUtils;
 import voldemort.xml.ClusterMapper;
 import voldemort.xml.StoreDefinitionsMapper;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.nio.file.Files;
 import java.util.List;
 
 /**
@@ -24,7 +30,7 @@ public class RebalanceControllerZooKeeper {
         boolean debug = true;
         // Bootstrap & fetch current cluster/stores
         String bootstrapURL = "tcp://192.168.0.210:6667";
-        String zkURL = "192.168.0.210:2181";
+        String zkURL = "192.168.0.210:2181/voldemort/config";
 
         //Rebalance parameters
         int parallelism = 8;
@@ -46,11 +52,13 @@ public class RebalanceControllerZooKeeper {
         ZooKeeperHandler zkHandler = new ZooKeeperHandler(zkURL);
         zkHandler.setupZooKeeper();
 
-        String finalClusterString = zkHandler.getStringFromZooKeeper("/knuttest/cluster_final.xml");
+        String finalClusterString = zkHandler.getStringFromZooKeeper("/cluster_final.xml");
         Cluster finalCluster = new ClusterMapper().readCluster(new StringReader(finalClusterString));
 
+        String interrimClusterString = zkHandler.getStringFromZooKeeper("/interrim_cluster.xml");
+
         List<StoreDefinition> finalStoreDefs;
-        String storesXML = zkHandler.getStringFromZooKeeper("/knuttest/stores.xml");
+        String storesXML = zkHandler.getStringFromZooKeeper("/stores.xml");
             finalStoreDefs = new StoreDefinitionsMapper().readStoreList(new StringReader(storesXML));
 
         //Validate stores across clusters
@@ -64,14 +72,25 @@ public class RebalanceControllerZooKeeper {
         System.out.println("Final clustername: " + finalCluster.getName());
         System.out.println("final store def: " + finalStoreDefs.get(0).getName());
 
-//        // Plan & execute rebalancing.
+
+
+        // Plan & execute rebalancing.
         rebalanceController.rebalance(new RebalancePlan(currentCluster,
                 currentStoreDefs,
                 finalCluster,
                 finalStoreDefs,
                 batchSize,
                 outputDir));
+
+        //Write cluster_final.xml to zookeeper as cluster.xml
+        zkHandler.uploadAndUpdateFile("/cluster.xml",finalClusterString);
+
+        //Uncomment to reset cluster.xml to interrim_cluster
+//        zkHandler.uploadAndUpdateFile("/cluster.xml",interrimClusterString);
     }
+
+
+
 
 
 
@@ -112,5 +131,33 @@ class ZooKeeperHandler{
             throw new RuntimeException(String.format("Error getting key from ZooKeeper: %s", path), e);
         }
         return s;
+    }
+
+    public byte[] getFileData(String path) {
+        try {
+            File file = new File(path);
+            if(file.isFile() && file.canRead()) {
+                byte[] fileData = Files.readAllBytes(file.toPath());
+                return fileData;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("error reading file data");
+        }
+        return null;
+    }
+
+    void uploadAndUpdateFile(String target, String content) {
+
+        try {
+            Stat stat = zk.exists(target, false);
+            if(stat == null) {
+                zk.create(target, content.getBytes(), null, CreateMode.PERSISTENT);
+            } else {
+                zk.setData(target, content.getBytes(), stat.getVersion());
+            }
+        } catch (InterruptedException | KeeperException e) {
+            e.printStackTrace();
+        }
     }
 }
