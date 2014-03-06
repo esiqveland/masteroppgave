2413a3579f53f6317a8870eced10a1536f48b9bc
knudix <knutnyg@gmail.com>
2014-03-06 16:49:23 +0100

    Moved out zookeeperhandler

--- voldemort/src/java/voldemort/tools/RebalanceControllerZooKeeper.java
+++ voldemort/src/java/voldemort/tools/RebalanceControllerZooKeeper.java
@@ -1,4 +1,5 @@
 package voldemort.tools;
+import org.apache.hadoop.mapred.jobfailures_jsp;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
@@ -79,14 +80,18 @@ public class RebalanceControllerZooKeeper {
 
 
         // Plan & execute rebalancing.
-        rebalanceController.rebalance(new RebalancePlan(currentCluster,
+        boolean failure = false;
+        try{
+            rebalanceController.rebalance(new RebalancePlan(currentCluster,
                 currentStoreDefs,
                 finalCluster,
                 finalStoreDefs,
                 batchSize,
                 outputDir));
+        } catch (Exception e){
+            failure = true;
+        }
 
-        boolean failure = false;
         if(failure){
             //Issue rollback of cluster.xml
             zkHandler.uploadAndUpdateFile("/cluster.xml", originalClusterString);
@@ -109,66 +114,3 @@ public class RebalanceControllerZooKeeper {
 }
 
 
-class ZooKeeperHandler{
-
-    private ZooKeeper zk;
-    private String zkURL;
-
-    public ZooKeeperHandler(String zkURL) {
-        this.zkURL = zkURL;
-
-
-    }
-
-    public void setupZooKeeper(){
-        zk = null;
-        try {
-            zk = new ZooKeeper(zkURL, 3000, null);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("zkurl failed to setup zookeeper");
-        }
-    }
-
-    public String getStringFromZooKeeper(String path){
-        Stat stat = new Stat();
-        String s = null;
-        try {
-            byte[] data = zk.getData(path, false, stat);
-            s = new String(data);
-        } catch (KeeperException e) {
-            throw new RuntimeException(String.format("Error getting key from ZooKeeper: %s", path), e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(String.format("Error getting key from ZooKeeper: %s", path), e);
-        }
-        return s;
-    }
-
-    public byte[] getFileData(String path) {
-        try {
-            File file = new File(path);
-            if(file.isFile() && file.canRead()) {
-                byte[] fileData = Files.readAllBytes(file.toPath());
-                return fileData;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("error reading file data");
-        }
-        return null;
-    }
-
-    void uploadAndUpdateFile(String target, String content) {
-
-        try {
-            Stat stat = zk.exists(target, false);
-            if(stat == null) {
-                zk.create(target, content.getBytes(), null, CreateMode.PERSISTENT);
-            } else {
-                zk.setData(target, content.getBytes(), stat.getVersion());
-            }
-        } catch (InterruptedException | KeeperException e) {
-            e.printStackTrace();
-        }
-    }
-}
