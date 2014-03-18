28676bb57862950a855fc20c59e88e166132f199
knudix <knudix@gmail.com>
2014-03-18 17:43:10 +0100

    -r

--- autoscale-client-start.sh
+++ autoscale-client-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/king/src/masteroppgaver/autoscale/autoscale-agent
+cd /home/king/src/masteroppgave/autoscale/autoscale-agent
 java -jar target/autoscale-agent-1.0.1-SNAPSHOT.jar
