bc24d001e8e1dbf11a88c33883701c5b3fcd514f
knudix <knudix@gmail.com>
2014-03-18 17:41:56 +0100

    change working directory beforehand

--- autoscale-client-start.sh
+++ autoscale-client-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-java -jar /home/king/src/masteroppgaver/autoscale/autoscale-agent/target/autoscale-agent-1.0.1-SNAPSHOT.jar
+cd /home/king/src/masteroppgaver/autoscale/autoscale-agent
+java -jar target/autoscale-agent-1.0.1-SNAPSHOT.jar
