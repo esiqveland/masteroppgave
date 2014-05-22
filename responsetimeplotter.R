198b7e67508bb0f2ddd1113f717b9a3bd66eb140
Eivind Siqveland Larsen <to.eivind@gmail.com>
2014-05-22 14:21:48 +0200

    Response plotting and parsing

---
+++ responsetimeplotter.R
@@ -0,0 +1,81 @@
+# plotter.R
+
+args<-commandArgs(TRUE)
+
+library('getopt') 
+
+spec <- matrix(c(
+        'zkdata'     , 'z', 1, "character", "zookeeperdata input file csv (required)",
+        'official'     , 'i', 1, "character", "input csv file for official version data (required)",
+        'output'    , 'o', 1, "character", "output filename (required)",
+        'help'   , 'h', 0, "logical",   "this help"
+),ncol=5,byrow=T)
+
+opt = getopt(spec);
+
+if ( !is.null(opt$help) || is.null(opt$official) || is.null(opt$zkdata) || is.null(opt$out) ) {
+    cat(paste(getopt(spec, usage=T),"\n"));
+    q(status=1);
+}
+
+#setup output
+pdf(opt$out)
+
+mydata = read.csv(opt$zkdata)
+# unpack histogram
+zookeeperdata <- unlist(apply(mydata, 1, function(x) rep(x[1], x[2])))
+
+
+mydatanozk = read.csv(opt$official)
+# unpack histogram
+nozk <- unlist(apply(mydatanozk, 1, function(x) rep(x[1], x[2])))
+
+
+
+
+responsetime = zookeeperdata#$responsetime
+
+par(xlog=FALSE)
+Fn = ecdf(responsetime)
+
+summary(nozk)
+sd(nozk)
+summary(zookeeperdata)
+sd(zookeeperdata)
+xrange = sd(nozk)^2 + 2*mean(nozk) + 45
+
+#plot(Fn, main="Response times", xlab="response time (ms)", ylab="cumulative response proportion", xlim=c(1,500), log="x")
+#plot(Fn, main="Response times", xlab="response time (ms)", ylab="cumulative response proportion", xlim=c(1,500), log="x")
+
+library(Hmisc)
+#g <- c( rep('Response Time (ms)',length(responsetime)) )
+x <- c(nozk, zookeeperdata)
+g <- c(rep('No ZooKeeper',length(nozk)),rep('ZooKeeper',length(zookeeperdata)))
+
+Ecdf(x, group=g, 
+	main="Response time distribution", 
+	xlab='Response Time (ms) ',
+	ylab = "percentile of requests",
+	datadensity="none", 
+	col=c('blue', 'red'),
+	lty=c(2,1),
+	lwd=c(2,1),
+	label.curves=list(keys=1:2),
+	#log="x",
+	q=c(.90,.999),
+	xlim=range(0,xrange)
+	)
+
+# second drawing
+# Ecdf(originalnozk, group=g, 
+# 	main="Response time distribution", 
+# 	xlab='Response Time ms ',
+	# ylab = "percentile of requests",
+	# datadensity="none", 
+	# add=TRUE,
+	# label.curves=TRUE,
+	# col='blue',
+	# #log="x",
+	# q=c(.50,.90,.999),
+	# xlim=range(0,5)
+	# )
---
+++ resultparser_single_nodes.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+
+import os
+import glob
+import sys
+import re
+import getopt
+
+
+def save_histogram(myhist, myoutfile):
+    myfile = open(myoutfile, "w")
+    myfile.write("responsetime, count\n")
+    for key, value in myhist.iteritems():
+        myfile.write("{},{}\n".format(key,value))
+        #for x in xrange(value):
+        #    myfile.write("{},1\n".format(key))
+
+def usage():
+    print "usage: "+sys.argv[0]+" -i inputdir -b benchmarks -p pattern -o outputfile" 
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "i:b:p:o:", ["help", "output="])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+
+    inputdir = "eivindmacbook"
+
+    #filestoparse = "nozk/*r99-w1*"
+    filestoparse = "zookeeper/*r99-w1*"
+
+    benchmarks = -1
+
+    outputfile = None
+
+    for o, a in opts:
+        if o == "-i":
+            inputdir = a
+            print o, a
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("-o", "--output"):
+            outputfile = a
+        elif o in ("-b", "--benchmarks"):
+            benchmarks = int(a)
+        elif o in ("-p", "--filepattern"):
+            filestoparse = a
+        else:
+            assert False, "unhandled option"
+
+    if not outputfile:
+        outputfile = inputdir+"/summary.csv"
+
+    print "settings:"
+    print "inputdir:", inputdir
+    print "benchmarks:", benchmarks
+    print "filestoparse:", filestoparse
+    print "output:", outputfile
+    paths = []
+
+    if benchmarks > -1:
+        for x in xrange(benchmarks):
+            mypath = inputdir+"/"+str(x)+"/"+filestoparse
+            paths += glob.glob(mypath)
+    else:
+        mypath = inputdir+"/"+filestoparse
+        paths += glob.glob(mypath)
+
+    print "files to parse:", paths
+
+    if len(paths) == 0:
+        print "no input files found"
+        sys.exit(-1)
+    
+    operations = {'reads': {}, 'writes': {}, 'updates': {}}
+    
+    pattern = re.compile(r'\[(?P<operation>\w+)\]\:\s+(?P<responsetime>\d+)\s+(?P<count>\d+)')
+
+    histogram = dict()
+    for datafile in paths:
+        with open(datafile, 'r') as myfile:
+            # skip warmup of data file
+            for line in myfile:
+#                if "iteration = 0" not in line:
+                if "[benchmark]" not in line:
+                    continue
+                break
+            next(myfile)
+            next(myfile)
+
+            for line in myfile:
+                # break when we reach the next iteration
+                if "iteration = 2" in line:
+                    break
+                result = pattern.match(line)
+                if result:
+                    result = result.groupdict()
+                    operation, responsetime, count = result["operation"], int(result["responsetime"]), int(result["count"])
+                    myoperationdict = operations[operation]
+                    if responsetime not in myoperationdict:
+                        myoperationdict[responsetime] = 0
+                        myoperationdict[responsetime] = myoperationdict[responsetime] + count
+
+    for myoperationdict in operations.values():
+        for responsetime, count in myoperationdict.iteritems():
+            if responsetime not in histogram:
+                histogram[responsetime] = 0
+            histogram[responsetime] += count
+
+    print "\ntotal operations tracked:", sum(histogram.values())
+    #print histogram
+
+    save_histogram(histogram, outputfile)
+
+if __name__ == "__main__":
+    main()
+
